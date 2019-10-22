4b4d6e6d7f2102e5e010cf227e09df51dd1536aa
Patrick Mahon <p.mahon0628@gmail.com>
2019-10-21 23:13:43 -0400

    learning some  basics

---
+++ app.py
@@ -0,0 +1,32 @@
+print ("What is up homie.")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# Variables
+character_name = "Pat"
+character_age = 29
+character_age_wish = "20"
+is_Male = True
+
+print("There once was a man named "  + character_name + ",")
+print("He is " + character_age + " years old")
+
+# reassign
+character_name = "Tim"
+
+print("There once was a man named "  + character_name + ",")
+print("He likes that his name is " + character_name + ",")
+print("He wishes he was " + character_age_wish + ".")
+
+# Strings
+
+phrase = "Umphreys Mcgee"
+print(phrase + "is cool")
+
+# functions
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.upper().isupper())
