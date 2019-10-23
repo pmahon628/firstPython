cee93558fa1b74523db147e93184b9f5eaf8e1c3
Patrick Mahon <p.mahon0628@gmail.com>
2019-10-22 22:39:21 -0400

    always learning more

--- app.py
+++ app.py
@@ -26,7 +26,59 @@ print("He wishes he was " + character_age_wish + ".")
 phrase = "Umphreys Mcgee"
 print(phrase + "is cool")
 
-# functions
+# functions for  strings
 print(phrase.lower())
 print(phrase.upper())
 print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("parmeter"))
+print(phrase.index("enter in param that is not in string throws  error "))
+print(phrase.replace("original", "replace"))
+
+# numbers
+print(2.0987)
+# cant print math functions
+print(3  * 4)
+# can also do order of operations
+print(5 * 5 + 4)
+print(3 * (4  + 5))
+# modulous - gives remainder - below  gives one
+print(10 % 3 )
+
+my_num = 5
+print(my_num)
+
+# convert # to string
+print(str(my_num) +  "  my  favorite number")
+
+# number functions
+# absolute value
+my_num =  -5
+print(abs)(my_num)
+# power to
+print(pow(3, 2))
+# max  - gives  bigger # / min - does opposite
+print(max(4 ,6))
+
+# importing - gives access to alot of math in python
+from math import *
+
+# getting input from user:
+name = input("Enter your name:  ")
+print("hello " + name + "!")
+
+age =  input("enter your age: ")
+print("Hello " + name + " You are " + age)
+
+# calculator
+num1 = input("enter a number: ")
+num2 = input("enter another number: ")
+result = int(num1) + int(num2) 
+# change int to float to get  decimals
+
+print(result)
+
+
+
+
