34524e1034292be5c3605f35717d973d7495e7c4
Patrick Mahon <p.mahon0628@gmail.com>
2020-01-01 10:36:19 -0500

    more functionality added

--- snake.py
+++ snake.py
@@ -27,6 +27,8 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+segments = []
+
 # functions
 
 def go_up():
@@ -69,6 +71,34 @@ wn.onkey(go_right, "d")
 # main game loop
 while True:
     wn.update()
+
+    # check for food collision
+    if head.distance(food) <20:
+        # move to random spot
+        x = random.radiant(-290, 290)
+        y = random.radiant(-290, 290)
+        food.goto(x,y)
+
+        # add new segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color(grey)
+        new_segment.penup()
+        segments.append(new_segment)
+    
+    # move end segments first in reverse order
+    for index in range(len(segments) -1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+        # move segment  0 to where the  head is
+        if len(segments) > 0:
+            x = head.xcor()
+            y = head.ycor()
+            segments[0].goto(x,y)
+
 
     move()
 
