4bdb01b3efc32f84a2c8066f7655fc8f2c722dfc
Patrick Mahon <p.mahon0628@gmail.com>
2020-01-01 11:03:23 -0500

    collision check set

--- snake.py
+++ snake.py
@@ -32,16 +32,20 @@ segments = []
 # functions
 
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
     if head.direction == "up":
@@ -72,6 +76,20 @@ wn.onkey(go_right, "d")
 while True:
     wn.update()
 
+    # check for collision with  the border
+    if  head.xcor()>290 or head.xcor()<-290 or head.ycor()>290  or  head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # hide the segments
+        for segment in segments:
+            segment.goto(1000,  1000)
+
+            # clear segments
+            segments.clear()
+
+
     # check for food collision
     if head.distance(food) <20:
         # move to random spot
@@ -101,6 +119,23 @@ while True:
 
 
     move()
+
+    # check for head collisions with body segments
+    for segment in segments:
+        if  segment.distance(head)  <  20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            # hide  the segments
+            for segment  in segments:
+                segment.goto(1000, 1000)
+
+            # clear the  segments  
+            segments.clear()
+
+
+
 
     time.sleep(delay)
 
